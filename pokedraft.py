93189918fb71be1c63e5f85c9e71f41ae771c2cb
Amlesh Sivanantham <samlesh@gmail.com>
2015-06-24 15:04:57 -0700

    Added an about page, with pikachu ascii art

--- pokedraft.py
+++ pokedraft.py
@@ -2,4 +2,53 @@ from pokedex import *
 from pokeDBmod import *
 
 pokeDex = PokeDex()
-pokeDex.mainMenu()
+
+
+def mainmenu():
+	clearScreen()
+	print "\n\nWelcome to ... "
+	about()
+
+def about():
+	print "quu..__"
+	print " $$$b  `---.__"
+	print "  \"$$b        `--.                          ___.---uuudP"
+	print "   `$$b           `.__.------.__     __.---'      $$$$\"              ."
+	print "     \"$b          -'            `-.-'            $$$\"              .'|"
+	print "       \".                                       d$\"             _.'  |"
+	print "         `.   /                              ...\"             .'     |"
+	print "           `./                           ..::-'            _.'       |"
+	print "            /                         .:::-'            .-'         .'"
+	print "           :                          ::''\\          _.'            |"
+	print "          .' .-.             .-.           `.      .'               |"
+	print "          : /'$$|           .@\"$\\           `.   .'              _.-'"
+	print "         .'|$u$$|          |$$,$$|           |  <            _.-'"
+	print "         | `:$$:'          :$$$$$:           `.  `.       .-'"
+	print "         :                  `\"--'             |    `-.     \\"
+	print "        :##.       ==             .###.       `.      `.    `\\"
+	print "        |##:                      :###:        |        >     >"
+	print "        |#'     `..'`..'          `###'        x:      /     /"
+	print "         \\                                   xXX|     /    ./"
+	print "          \\                                xXXX'|    /   ./"
+	print "          /`-.                                  `.  /   /"
+	print "         :    `-  ...........,                   | /  .'"
+	print "         |         ``:::::::'       .            |<    `."
+	print "         |             ```          |           x| \ `.:``."
+	print "         |                         .'    /'   xXX|  `:`M`M':."
+	print "         |    |                    ;    /:' xXXX'|  -'MMMMM:'"
+	print "         `.  .'                   :    /:'       |-'MMMM.-'"
+	print "          |  |                   .'   /'        .'MMM.-'"
+	print "          `'`'                   :  ,'          |MMM<"
+	print "            |                     `'            |tbap\\"
+	print "             \\                                  :MM.-'"
+	print "              \\                 |              .''"
+	print "               \\.               `.            /"
+	print "                /     .:::::::.. :           /"
+	print "               |     .:::::::::::`.         /"
+	print "               |   .:::------------\       /"
+	print "              /   .''               >::'  /"
+	print "              `',:                 :    .'"
+	print "  SOURCE:chris.com/ASCII                  "
+
+mainmenu()
+
