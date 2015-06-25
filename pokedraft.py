f338a4f76db80965799246616812f3b1da18806d
Amlesh Sivanantham <samlesh@gmail.com>
2015-06-24 23:29:51 -0700

    Added more menu functionality to the draft menu

--- pokedraft.py
+++ pokedraft.py
@@ -1,13 +1,69 @@
 from pokedex import *
 from pokeDBmod import *
+import random
 
 pokeDex = PokeDex()
 
+def draftMenu():
+	pass
 
-def mainmenu():
-	clearScreen()
-	print "\n\nWelcome to ... "
+def mainMenu():
+	loadSettings()
+	pokeDex = PokeDex()
+	while True:
+		clearScreen()
+		print "\n\nWelcome to \n"
+		printTitle()
+		print "\n Please choose an option."
+		print "\n\t1. Start Pokemon Draft"
+		print "\t2. View PokeDex"
+		print "\t3. About"
+		print "\t3. Settings"
+		choice = str(raw_input("\n>>> "))
 
+		if choice == "1":
+			draftMenu()
+		elif choice == "2":
+			pokeDex.mainMenu()
+		elif choice == "3":
+			about()
+		elif choice == "4":
+			pass
+		else:
+			raw_input("\nNot a valid option.")
+
+
+
+def loadSettings():
+	pass
+
+def printTitle():
+	x = random.randrange(1,3)
+	if x == 1:
+		print ";-.      ,                     "
+		print "|  )     |                     "
+		print "|-'  ,-. | , ,-. ;-.-. ,-. ;-. "
+		print "|    | | |<  |-' | | | | | | | "
+		print "'    `-' ' ` `-' ' ' ' `-' ' '" 
+		print " "                               
+		print ",-.              .      "       
+		print "|  \          ,- |     "        
+		print "|  | ;-. ,-:  |  |-   "         
+		print "|  / |   | |  |- |   "          
+		print "`-'  '   `-`  |  `-'"           
+		print "             -'    "
+	elif x == 2:
+		print "   ___      _                              "
+		print "  / _ \___ | | _____ _ __ ___   ___  _ __  "
+		print " / /_)/ _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \ "
+		print "/ ___/ (_) |   <  __/ | | | | | (_) | | | |"
+		print "\/    \___/|_|\_\___|_| |_| |_|\___/|_| |_| "
+		print "    ___            __ _                    "
+		print "   /   \_ __ __ _ / _| |_                  "
+		print "  / /\ / '__/ _` | |_| __|                 "
+		print " / /_//| | | (_| |  _| |_                  "
+		print "/___,' |_|  \__,_|_|  \__|                 "
+                           
 def about():
 	print "quu..__"
 	print " $$$b  `---.__"
@@ -48,6 +104,7 @@ def about():
 	print "              /   .''               >::'  /"
 	print "              `',:                 :    .'"
 	print "  SOURCE:chris.com/ASCII                  "
+	raw_input("\nPress Enter to Continue...")
 
-mainmenu()
+mainMenu()
 
